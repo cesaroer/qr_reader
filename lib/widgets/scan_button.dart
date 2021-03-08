25c4883498db354455540184f2b75ab9aa73a8b3
cesaroer <cesarvarcard@gmail.com>
2021-03-07 23:40:33 -0600

    urlLaucher now launchs since scanButton

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:provider/provider.dart';
+import 'package:qr_reader/Utils/utils.dart';
 import 'package:qr_reader/providers/db_provider.dart';
 import 'package:qr_reader/providers/scan_list_provider.dart';
 
@@ -11,13 +12,19 @@ class ScanButton extends StatelessWidget {
       elevation: 0,
       onPressed: () async {
         //String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode("#3D8BEF", "Cancelar", false, ScanMode.QR);
-        final barcodeScanRes = "https://www.google.com";
+        //final barcodeScanRes = "https://www.google.com";
+        final barcodeScanRes = "geo:19.322572,-99.267815";
+
+        if (barcodeScanRes == -1) {
+          return;
+        }
 
         final scanListProvider =
             Provider.of<ScanListService>(context, listen: false);
 
-        scanListProvider.nuevoScan(barcodeScanRes);
-        scanListProvider.nuevoScan("geo:15.33,15.66");
+        final nuevoScan = await scanListProvider.nuevoScan(barcodeScanRes);
+
+        launchURL(context, nuevoScan);
       },
       child: Icon(Icons.filter_center_focus),
     );
